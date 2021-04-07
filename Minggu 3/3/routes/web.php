d551e65af468a9eb1d9da59a3ecee11891c89422
Daniel Saputro <udanieru@gmail.com>
2021-04-07 18:29:04 +0700

    adding Tugas 3 Laravel Blade

---
+++ Minggu 3/3/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/table', function () {
+    return view('table');
+})/*->name('table')*/;
+
+Route::get('/datatable', function () {
+    return view('datatable');
+});
