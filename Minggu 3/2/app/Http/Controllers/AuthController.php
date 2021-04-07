a3ca8f8ddd7dc1819ba2ec15c0eab97ce58f04c3
Daniel Saputro <udanieru@gmail.com>
2021-04-07 12:44:21 +0700

    update for Intro Laravel

--- Minggu 3/2/app/Http/Controllers/AuthController.php
+++ Minggu 3/2/app/Http/Controllers/AuthController.php
@@ -14,7 +14,6 @@ class AuthController extends Controller
         $first = $request["first"];
         $last = $request["last"];
 
-        return "<h1>Selamat Datang $first $last</h1>
-        <h2>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h2>";
+        return view('welcome', ['firstname' => $first, 'lastname' => $last]);
     }
 }
