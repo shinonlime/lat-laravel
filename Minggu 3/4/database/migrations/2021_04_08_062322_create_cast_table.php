ad293b66d4c8af5f6ab51607fee0cda1b1b9d0b2
Daniel Saputro <udanieru@gmail.com>
2021-04-08 13:46:05 +0700

    adding Tugas 4 Database & Migration Laravel

---
+++ Minggu 3/4/database/migrations/2021_04_08_062322_create_cast_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCastTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cast', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('umur');
+            $table->text('bio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cast');
+    }
+}
